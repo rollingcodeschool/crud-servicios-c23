2546c7b70400dec31b7c3a6848a16a44ce57e49e
Emilse Arias <emilsearias@gmail.com>
2026-05-04 11:39:11 -0300

    agregamos las rutas del login, admin y la ruta principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
-import Error404 from "./components/pages/Error404"
-import Inicio from "./components/pages/Inicio"
-import Login from "./components/pages/Login"
-import Footer from "./components/shared/Footer"
-import Menu from "./components/shared/Menu"
+import Error404 from "./components/pages/Error404";
+import Inicio from "./components/pages/Inicio";
+import Administrador from "./components/pages/Administrador";
+import Login from "./components/pages/Login";
+import Footer from "./components/shared/Footer";
+import Menu from "./components/shared/Menu";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 function App() {
-
   return (
-   <div className="min-h-screen bg-zinc-950 text-zinc-100 flex flex-col">
-    <Menu/>
-    <main className="container mx-auto">
-    {/* <Inicio></Inicio> */}
-    {/* <Error404></Error404> */}
-    {/* <Login></Login> */}
-    </main>
-    <Footer/>
-   </div>
-
-  )
+    <BrowserRouter>
+      <div className="min-h-screen bg-zinc-950 text-zinc-100 flex flex-col">
+        <Menu />
+        <main className="container mx-auto">
+          <Routes>
+            <Route path="/" element={<Inicio></Inicio>}/>
+            <Route path="/login" element={<Login></Login>}/>
+            <Route path="/administrador" element={<Administrador></Administrador>}/>
+            <Route path="*" element={<Error404></Error404>}/>
+          </Routes>
+          {/*  */}
+          {/* <Error404></Error404> */}
+          {/* <Login></Login> */}
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
