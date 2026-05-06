a085370fe8974d00b1ca2d198e4887eeef559a66
Emilse Arias <emilsearias@gmail.com>
2026-05-05 23:12:01 -0300

    agregue el componete FormularioServicio y las rutas faltantes en App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Error404 from "./components/pages/Error404";
 import Inicio from "./components/pages/Inicio";
 import Administrador from "./components/pages/Administrador";
+import FormularioServicio from "./components/pages/FormularioServicio";
 import Login from "./components/pages/Login";
 import Footer from "./components/shared/Footer";
 import Menu from "./components/shared/Menu";
@@ -11,11 +12,13 @@ function App() {
     <BrowserRouter>
       <div className="min-h-screen bg-zinc-950 text-zinc-100 flex flex-col">
         <Menu />
-        <main className="container mx-auto">
+        <main className="container mx-auto px-4 py-8">
           <Routes>
             <Route path="/" element={<Inicio></Inicio>}/>
             <Route path="/login" element={<Login></Login>}/>
             <Route path="/administrador" element={<Administrador></Administrador>}/>
+            <Route path="/administrador/crear" element={<FormularioServicio titulo={'Crear Servicio'}></FormularioServicio>}/>
+            <Route path="/administrador/editar/:id" element={<FormularioServicio titulo={'Editar Servicio'}></FormularioServicio>}/>
             <Route path="*" element={<Error404></Error404>}/>
           </Routes>
           {/*  */}
