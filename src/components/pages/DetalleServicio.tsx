5d6384fe30b2a2a1e7850b3eef9b74d08fbfd6ce
Emilse Arias <emilsearias@gmail.com>
2026-05-18 09:40:00 -0300

    deje solo el maquetado de la descripcion

--- src/components/pages/DetalleServicio.tsx
+++ src/components/pages/DetalleServicio.tsx
@@ -5,39 +5,23 @@ import { useEffect } from "react";
 
 const DetalleServicio = () => {
     const { id } = useParams<{ id: string }>();
-    const { buscarServicio } = useAppContext();
-    const navigate = useNavigate();
-
-    // Buscar el servicio por id
-    const servicio = id ? buscarServicio(id) : undefined;
-
-    useEffect(() => {
-        if (!servicio) {
-            // Si no existe el servicio, redirigir a 404
-            navigate("/404");
-        }
-    }, [servicio, navigate]);
-
-    if (!servicio) {
-        return null; 
-    }
-
+  
     return (
         <div className="max-w-xl mx-auto bg-zinc-900 rounded-lg shadow-lg p-8 mt-8">
-            <h2 className="text-3xl font-bold mb-4 text-center">{servicio.nombreServicio}</h2>
+            <h2 className="text-3xl font-bold mb-4 text-center">Nombre de servicio</h2>
             <img
-                src={servicio.imagen}
-                alt={servicio.nombreServicio}
+                src=''
+                alt=''
                 className="w-full h-64 object-cover rounded mb-4 border border-zinc-700"
             />
             <p className="text-lg mb-2">
-                <span className="font-semibold">Precio:</span> ${servicio.precio}
+                <span className="font-semibold">Precio:</span> $00,00
             </p>
             <p className="text-lg mb-2">
-                <span className="font-semibold">Categoría:</span> {servicio.categoria}
+                <span className="font-semibold">Categoría:</span> categoria
             </p>
             <p className="mb-4">
-                <span className="font-semibold">Descripción:</span> {servicio.descripcion}
+                <span className="font-semibold">Descripción:</span>descripcion del servicio
             </p>
             <Link
                 to="/"
