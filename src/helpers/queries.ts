ae63b59b25cdad7d0f48f5adcd6e4e4d56ce0da6
Emilse Arias <emilsearias@gmail.com>
2026-07-01 11:28:06 -0300

    agregamos el archivo de queries con las consultas para listar y crear un servicio

---
+++ src/helpers/queries.ts
@@ -0,0 +1,29 @@
+import type { Servicio } from "../interfaces/servicios";
+
+const urlServicios = import.meta.env.VITE_SERVICIO
+
+export const listarServiciosApi = async ():Promise<Response> =>{
+    try{
+        const respuesta = await fetch(urlServicios)
+        return respuesta
+    }catch(error){
+        console.error(error)
+        throw error
+    }
+};
+
+export const crearServicioApi = async (servicio: Servicio):Promise<Response> =>{
+    try{
+        const respuesta = await fetch(urlServicios, {
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(servicio)
+        })
+        return respuesta
+    }catch(error){
+        console.error(error)
+        throw error
+    }
+};
